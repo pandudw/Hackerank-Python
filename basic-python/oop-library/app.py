094e90d5812c70e2597fbe9226c28a9c42693609
Nasrullah Pandu <103873184+pandudw@users.noreply.github.com>
2024-11-02 19:03:22 +0000

    exercise python oop library

---
+++ basic-python/oop-library/app.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self,title,author,year):
+        self.title  = title
+        self.author = author
+        self.year   = year
+    def __str__(self):
+        return f'"{self.title}" by {self.author} in {self.year}'
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self,book):
+        self.books.append(book)
+        print("Added:", {book})
+    
+    def __display_book(self):
+        if not self.books:
+            print("No book available")
+            return
+
+        print("Book available in this library")
+        for book in self.books:
+            print(book)
+
+
